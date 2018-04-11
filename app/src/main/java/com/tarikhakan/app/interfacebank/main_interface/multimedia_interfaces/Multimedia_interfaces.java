f3ef3b56bb8b5fc1fe32404137f89afdec3781ce
tarikhakan55 <tarikhakan55@gmail.com>
2018-04-12 00:09:05 +0300

    interface 1 github source

--- app/src/main/java/com/tarikhakan/app/interfacebank/main_interface/multimedia_interfaces/Multimedia_interfaces.java
+++ app/src/main/java/com/tarikhakan/app/interfacebank/main_interface/multimedia_interfaces/Multimedia_interfaces.java
@@ -60,7 +60,7 @@ public class Multimedia_interfaces extends AppCompatActivity {
             goGithub.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com"));
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/tarikhakan55/InterfaceBank/blob/master/app/src/main/assets/main_interface/multimedia_interfaces/interfaces/interface_1/Interface%201%20Sources"));
                     startActivity(browserIntent);
                 }
             });
